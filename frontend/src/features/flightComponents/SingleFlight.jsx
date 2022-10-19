ea3b6eae37d8c0c36154273c7ff597f7e9140acc
Muhammed najeeb <muhdnajee3@gmail.com>
2022-10-19 19:17:48 +0530

    new

--- frontend/src/features/flightComponents/SingleFlight.jsx
+++ frontend/src/features/flightComponents/SingleFlight.jsx
@@ -9,6 +9,8 @@ import {
 } from "../flightBookingComponents/flightBookingSlice";
 import { useDispatch } from "react-redux";
 import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 export const SingleFlight = ({
   arrival,
   arrival_time,
@@ -19,10 +21,9 @@ export const SingleFlight = ({
   name,
   stops,
   _id,
-  Origin
-  
+  Origin,
 }) => {
-  const [viewPrices,setViewPrices] = useState(false)
+  const [viewPrices, setViewPrices] = useState(false);
   const dispatch = useDispatch();
   const handlePageChange = (id) => {
     dispatch(flightBookingLoading());
@@ -38,16 +39,30 @@ export const SingleFlight = ({
     navigate(`/flightBooking/${id}`);
   };
   const VIEWPRICES = (_id) => {
-    if(_id === viewPrices){
-    setViewPrices(true)
-
+    if (_id === viewPrices) {
+      setViewPrices(true);
+    }
+  };
+  const buttonactionondiv = (id) => {
+     if (window.innerWidth<580) {
+      dispatch(flightBookingLoading());
+    fetch(`https://makemytripback.herokuapp.com/flights/${id}`)
+      .then((r) => r.json())
+      .then((r) => {
+        dispatch(addBookingFlights(r));
+      })
+      .catch((e) => {
+        console.log(e);
+        dispatch(flightBookingError());
+      });
+    navigate(`/flightBooking/${id}`);
     }
   }
   const navigate = useNavigate();
   return (
     <>
-    <div className={styles.singleflight} key={_id}>
-      {/* <div
+      <div className={styles.singleflight} key={_id} onClick={() => buttonactionondiv(_id)}>
+        {/* <div
         style={{
           backgroundColor: "#B8FFF9",
           padding: "0.2px",
@@ -57,74 +72,82 @@ export const SingleFlight = ({
         }}
         >
         <h6 className={styles.guidelineText}>Lock price and pay later</h6>
-      </div> */}
-      <div className={styles.maincontainer}>
-        <div
-          className={styles.inner_cont}
-          style={{
-            fontSize: "14px",
-            fontWeight: "700",
-            marginTop: "2%",
-            marginLeft: "1%",
-          }}
-          >
-          <span>{name}</span>
-        </div>
-
-        <div className={styles.inner_cont}>
-          <b>{departure_time}</b>
-          <p>{departure}</p>
-        </div>
-        <div className={styles.inner_cont}>
-          <span>{duration}</span>
-          <hr className={styles.line} />
-          <span
+        </div> */}
+        <div className={styles.maincontainer}>
+          <div
+            className={styles.inner_cont}
             style={{
-              fontSize: "12px",
-              color: "gray",
-              textAlign: "center",
+              fontSize: "14px",
+              fontWeight: "700",
+              marginTop: "2%",
+              marginLeft: "1%",
+              display:"flex",
+              flexDirection:"row",
+              // width:"12%"
             }}
-            >
-            {stops}
-          </span>
-        </div>
-        <div className={styles.inner_cont}>
-          <b>{arrival_time}</b>
-          <p>{arrival}</p>
-        </div>
-        <div className={styles.inner_cont}>
-          <b>{fare}/-</b>
-        </div>
-        <Button
-          className={styles.buttonBookNow}
-          style={{
-            maxHeight: "30px",
-            marginTop: "1.5%",
-            fontSize: "10px",
-            backgroundColor: "#9AD0EC",
-            border: "1px solid #03045E",
-            width: "10%",
-          }}
-          onClick={() => handlePageChange(_id)}
-          // onClick={()=>VIEWPRICES(_id)}
           >
-          Book Now
-        </Button>
-      </div>
-      {/* <div className={styles.discount}>
-        Use MMTSUPER and get FLAT Rs. 309 instant discount on this flight
-      </div> */}
-      <div className={styles.details}>
-        <span>View Flight Details</span>
-      </div>
-    </div> 
-    {/* {viewPrices.map((item) => (
-        <>
-      <h1>hi</h1>
-      </>
-      ))} */}
-        
+            {/* <FontAwesomeIcon icon={faUser} /> Login in to view */}
+            <img src="	https://imgak.mmtcdn.com/flights/assets/media/mobile/common/hdpi/6E.png?v=9" className={styles.imgairline}/>
+            <span style={{marginLeft:"5px"}}>{name}</span>
+          </div>
+          <div className={styles.divsec}>
+            <div className={styles.inner_cont}>
+              <b>{departure_time}</b>
+              <p>{departure}</p>
+            </div>
+            <div className={styles.inner_cont}>
+              <span>{duration}</span>
+              <hr className={styles.line} />
+              <span
+                style={{
+                  fontSize: "12px",
+                  color: "gray",
+                  // textAlign: "center",
+                }}
+              >
+                {stops}
+              </span>
+            </div>
+            <div className={styles.inner_cont}>
+              <b>{arrival_time}</b>
+              <p>{arrival}</p>
+            </div>
+            <div className={styles.inner_cont}>
+              <b>{fare}/-</b>
+            </div>
+            <div className={styles.btnbook}>
 
-      </>
+            <Button
+              className={styles.buttonBookNow}
+              style={{
+                maxHeight: "30px",
+                marginTop: "1.5%",
+                fontSize: "10px",
+                // backgroundColor: "#9AD0EC",
+                border: "1px solid #03045E",
+                width: "10%",
+              }}
+              onClick={() => handlePageChange(_id)}
+              // onClick={()=>VIEWPRICES(_id)}
+              >
+              Book Now
+            </Button>
+              </div>
+            </div>
+          {/* <div className={styles.discount}>
+          Use MMTSUPER and get FLAT Rs. 309 instant discount on this flight
+          </div> */}
+          
+        </div>
+        <div className={styles.details}>
+            <span>View Flight Details</span>
+          </div>
+      </div>
+      {/* {viewPrices.map((item) => (
+        <>
+        <h1>hi</h1>
+        </>
+      ))} */}
+    </>
   );
 };
